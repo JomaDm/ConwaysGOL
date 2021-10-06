092518570cda5e6047bf74637ae5fa370217dde7
Joma Dm <Joma_Dm@outlook.com>
2021-10-06 12:16:42 -0500

    Cambios en importacion css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import Board from './Board';
 
-const AppContainer = styled.div`
+const AppContainer = createGlobalStyle`
 	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 	width: 100%;
 	padding: 10vh 10vw;
